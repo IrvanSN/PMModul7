b9e853f00bf8a1c0b9672d26153d0f40fec7d246
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-11-27 20:44:09 +0700

    base code project

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// package @expo/vector-icons perlu di install agar bisa menggunakan icon
+import { FontAwesome5 } from '@expo/vector-icons';
+import { AboutScreen, TaskCompletedScreen, TaskScreen } from './screens';
+import FlashMessage from 'react-native-flash-message';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const BottomNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Task') {
+            iconName = 'tasks';
+          } else if (route.name === 'Completed') {
+            iconName = 'clipboard-check';
+          } else if (route.name === 'About') {
+            iconName = 'exclamation-circle';
+          }
+          return (
+            <FontAwesome5
+              name={iconName}
+              size={size}
+              color={focused ? 'navy' : color}
+            />
+          );
+        },
+        tabBarIconStyle: { marginTop: 10 },
+        tabBarLabel: ({ children, color, focused }) => {
+          return (
+            <Text style={{ marginBottom: 10, color: focused ? 'navy' : color }}>
+              {children}
+            </Text>
+          );
+        },
+        tabBarStyle: {
+          height: 70,
+          borderTopWidth: 0,
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Task"
+        component={TaskScreen}
+        options={{ title: 'All Task', unmountOnBlur: true }}
+      />
+      <Tab.Screen
+        name="Completed"
+        component={TaskCompletedScreen}
+        options={{ unmountOnBlur: true }}
+      />
+      <Tab.Screen
+        name="About"
+        component={AboutScreen}
+        options={{ unmountOnBlur: true }}
+      />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="BottomNavigator"
+            component={BottomNavigator}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <FlashMessage position="top" />
+    </>
+  );
+};
+
+export default App;
